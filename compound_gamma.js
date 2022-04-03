df577543ec61abe7edb210014bf8665b468fe305
Craig Kilgo <craigkilgo@gmail.com>
2022-04-03 17:39:43 -0400

    google sheet-ify

--- compound_gamma.js
+++ compound_gamma.js
@@ -3,14 +3,14 @@ function compound_gamma_dist(x,a,b,q,cumulative){
     if(cumulative==false){
         compound_gamma_dist = (x * q) ^ (a - 1) * (1 + x * q) ^ (-a - b) / (1 / q * beta_function(a, b));
     }else{
-        compound_gamma_dist = WorksheetFunction.Beta_Dist(1 / (1 + (1 / q) / x), a, b, true);
+        compound_gamma_dist = BETA.DIST(1 / (1 + (1 / q) / x), a, b, true);
     }
     return compound_gamma_dist;
 }
 
 function beta_function(a,b){
     var beta_function;
-    beta_function = Math.exp(WorksheetFunction.GammaLn(a) + WorksheetFunction.GammaLn(b) - WorksheetFunction.GammaLn(a + b))
+    beta_function = Math.exp(GAMMALN(a) + GAMMALN(b) - GAMMALN(a + b))
 
     return beta_function;
 }
