747a737e426a023848e0c21065aed006ec6d7f4b
Craig Kilgo <craigkilgo@gmail.com>
2022-04-26 13:16:59 -0400

    combin

--- compound_gamma.js
+++ compound_gamma.js
@@ -46,7 +46,31 @@ function GAMMALN(a){
     return Math.log(math.gamma(a));
 }
 
+//combin
 
+function product_Range(a,b) {
+    var prd = a,i = a;
+   
+    while (i++< b) {
+      prd*=i;
+    }
+    return prd;
+  }
+  
+  
+  function Combin(n, r) 
+  {
+    if (n==r || r==0) 
+    {
+      return 1;
+    } 
+    else 
+    {
+      r=(r < n-r) ? n-r : r;
+      return product_Range(r+1, n)/product_Range(1,n-r);
+    }
+  }
+  
 
 //beta
 
